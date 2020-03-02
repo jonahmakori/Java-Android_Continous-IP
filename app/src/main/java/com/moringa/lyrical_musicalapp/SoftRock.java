a23b258c696cb98cba63cbe31c26c0f0b93a5d6f
jonahmakori <jonahmakori0@gmail.com>
2020-03-02 15:18:22 +0300

    Created a hiphop and a soft rock activity for the music categories array

--- app/src/main/java/com/moringa/lyrical_musicalapp/SoftRock.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/SoftRock.java
@@ -1,4 +1,27 @@
 package com.moringa.lyrical_musicalapp;
 
-class SoftRock {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SoftRock extends AppCompatActivity {
+    @BindView(R.id.softrock) TextView mSoftRock;
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft_rock);
+
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String musicGenres = intent.getStringExtra("musicGenres");
+        mSoftRock.setText("These is HipHop Music " );
+    }
 }
