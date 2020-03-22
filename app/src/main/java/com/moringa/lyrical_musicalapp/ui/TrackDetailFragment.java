8c2249b490afa18d0d466579a2463aa3724f8220
jonahmakori <jonahmakori0@gmail.com>
2020-03-22 16:36:22 +0300

    Created an Intent for the TrackShareUrl for Lyrics to take the user to view full lyrics

--- app/src/main/java/com/moringa/lyrical_musicalapp/ui/TrackDetailFragment.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/ui/TrackDetailFragment.java
@@ -1,50 +1,50 @@
 package com.moringa.lyrical_musicalapp.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.moringa.lyrical_musicalapp.Constants;
 import com.moringa.lyrical_musicalapp.R;
-import com.moringa.lyrical_musicalapp.models.Track;
+import com.moringa.lyrical_musicalapp.models.TrackList;
 
 import org.parceler.Parcels;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TrackDetailFragment extends Fragment implements View.OnClickListener{
 
-    @BindView(R.id.textView2)
-    ImageView mImageLabel;
-    @BindView(R.id.restaurantNameTextView)
-    TextView mNameLabel;
-    @BindView(R.id.ratingTextView) TextView mRatingLabel;
-    @BindView(R.id.websiteTextView) TextView mWebsiteLabel;
-    @BindView(R.id.phoneTextView) TextView mPhoneLabel;
-    @BindView(R.id.addressTextView) TextView mAddressLabel;
-    @BindView(R.id.saveSearcTrackButton) TextView mSaveSearchTrackButton;
 
-    private Track mTracks;
+    @BindView(R.id.trackNameTextView)
+    TextView mTrackNameTextView;
+    @BindView(R.id.fragmentRatingTextView) TextView mRatingTextView;
+    @BindView(R.id.lyricsTextView) TextView mLyricsTextView;
+    @BindView(R.id.fragmentAlbumName) TextView mFragmentAlbumName;
+    @BindView(R.id.saveTrackButton) Button mSaveTrackButton;
+    @BindView(R.id.artistNameTextView) TextView mArtistNameTextView;
+
+    private TrackList mTracks;
 
     public TrackDetailFragment() {
         // Required empty public constructor
     }
 
-    public static TrackDetailFragment newInstance(Track track) {
+    public static TrackDetailFragment newInstance(TrackList track) {
         TrackDetailFragment trackDetailFragment = new TrackDetailFragment();
         Bundle args = new Bundle();
-        args.putParcelable("q_track", Parcels.wrap(track));
+        args.putParcelable("tracks", Parcels.wrap(track));
         trackDetailFragment.setArguments(args);
         return trackDetailFragment;
     }
@@ -55,9 +55,10 @@ public class TrackDetailFragment extends Fragment implements View.OnClickListene
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mTracks = Parcels.unwrap(getArguments().getParcelable("q_artist"));
+        mTracks = Parcels.unwrap(getArguments().getParcelable("tracks"));
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_track_detail, container, false);
@@ -65,45 +66,30 @@ public class TrackDetailFragment extends Fragment implements View.OnClickListene
 
         //Picasso.get().load(mTracks.getLyricsId()).into(mImageLabel);
 
-        List<Class<Locale.Category>> categories = new ArrayList<>();
 
-        for (Locale.Category category: mSaveSearchTrackButton()) {
-            categories.add(category.getDeclaringClass());
-        }
 
-        //mNameLabel.setText(mTracks.);
-        //mSaveSearchTrackButton.setText(android.text.TextUtils.join(", ", categories));
-        //mRatingLabel.setText(Double.toString(mTracks.getHasLyrics()) + "/5");
-        //mPhoneLabel.setText(mTracks.getTrackSpotifyId());
-//        mAddressLabel.setText(mTracks.getHasLyrics().toString());
-        //mWebsiteLabel.setOnClickListener(this);
-        //mPhoneLabel.setOnClickListener(this);
-        //mAddressLabel.setOnClickListener(this);
+        mRatingTextView.setText(Double.toString(mTracks.getTrack().getTrackRating()) + "/35");
+        mTrackNameTextView.setText(mTracks.getTrack().getTrackName());
+        mFragmentAlbumName.setText(mTracks.getTrack().getAlbumName());
+        mArtistNameTextView.setText(mTracks.getTrack().getArtistName());
+        mLyricsTextView.setOnClickListener(this);
+        mSaveTrackButton.setOnClickListener(this);
         return view;
     }
 
-    private Locale.Category[] mSaveSearchTrackButton() {
-        return mSaveSearchTrackButton();
-    }
 
 
     @Override
     public void onClick(View v){
-        if (v == mWebsiteLabel){
-            Intent webIntent = new Intent (Intent.ACTION_VIEW, Uri.parse(mTracks.getTrackEditUrl()));
+        if (v == mLyricsTextView){
+            Intent webIntent = new Intent (Intent.ACTION_VIEW, Uri.parse(mTracks.getTrack().getTrackShareUrl()));
             startActivity(webIntent);
         }
-        if (v == mPhoneLabel) {
-            Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + mTracks.getTrackShareUrl()));
-            startActivity(phoneIntent);
+        if(v == mSaveTrackButton){
+            DatabaseReference trackRef = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_TRACKS);
+            trackRef.push().setValue(mTracks);
+            Toast.makeText(getContext(), "Saved Track", Toast.LENGTH_SHORT).show();
         }
-        if (v == mAddressLabel){
-            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + mTracks.getHasLyrics()
-                    + "," + mSaveSearchTrackButton.getAutoLinkMask() + "?q=(" + mSaveSearchTrackButton.getBaseline() +")"));
-            startActivity(mapIntent);
-        }
-
-
 
     }
 }
