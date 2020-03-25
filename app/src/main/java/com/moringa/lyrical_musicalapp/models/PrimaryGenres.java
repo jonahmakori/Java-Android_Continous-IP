241f1e6d1c0d24636176dc9feb806ea905625fed
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 11:47:20 +0300

    Added a Music Genre for all categories from  the the json

--- app/src/main/java/com/moringa/lyrical_musicalapp/models/PrimaryGenres.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/models/PrimaryGenres.java
@@ -1,10 +1,17 @@
 
 package com.moringa.lyrical_musicalapp.models;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.parceler.Parcel;
+
+import java.util.List;
+
+
+
+
+@Parcel
 public class PrimaryGenres {
 
     @SerializedName("music_genre_list")
