be4159b851fc57653f486f96c96aea9a2a63a40f
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 10:43:39 +0300

    Extracted the String Values in strings.xml

---
+++ app/src/main/java/com/moringa/lyrical_musicalapp/adapters/FireBaseTrackAdapter.java
@@ -0,0 +1,4 @@
+package com.moringa.lyrical_musicalapp.adapters;
+
+public class FireBaseTrackAdapter {
+}
---
+++ app/src/main/java/com/moringa/lyrical_musicalapp/ui/SavedTracksActivity.java
@@ -0,0 +1,12 @@
+package com.moringa.lyrical_musicalapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedTracksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
