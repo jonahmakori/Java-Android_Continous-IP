afb74561a37762769351b3a23f699bff3171c8f7
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 11:29:00 +0300

    CAlled the lyrics endpoint and passed in the q_artist and q_track

--- app/src/main/java/com/moringa/lyrical_musicalapp/network/MusixmatchApi.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/network/MusixmatchApi.java
@@ -1,5 +1,6 @@
 package com.moringa.lyrical_musicalapp.network;
 
+
 import com.moringa.lyrical_musicalapp.Constants;
 import com.moringa.lyrical_musicalapp.models.MusixmatchTrackSearchResponse;
 
@@ -9,12 +10,15 @@ import retrofit2.http.Query;
 
 public interface MusixmatchApi {
     @GET("/ws/1.1/track.search&" + "apikey=" + Constants.MUSIXMATCH_API_KEY)
-    Call<MusixmatchTrackSearchResponse> getTrack(
-//            @Path("apiKey") String apiKey,
-//            @Url String url,
+    Call<MusixmatchTrackSearchResponse> getTracks(
             @Query("q_track") String q_track,
             @Query("q_artist") String q_artist
-
-
     );
+
+//        @GET("/ws/1.1/matcher.lyrics.get&" + "apikey=" + Constants.MUSIXMATCH_API_KEY)
+//    Call<MusixmatchTrackSearchResponse2> getLyrics(
+//            @Query("q_track") String q_track,
+//            @Query("q_artist") String q_artist
+//    );
+
 }
