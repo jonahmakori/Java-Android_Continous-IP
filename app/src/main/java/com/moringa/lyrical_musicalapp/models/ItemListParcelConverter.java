fea580a837eff2d7362001692bc60601edcd6763
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 12:06:18 +0300

    Added the ItemList and  String converters for the models classes

--- app/src/main/java/com/moringa/lyrical_musicalapp/models/ItemListParcelConverter.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/models/ItemListParcelConverter.java
@@ -1,6 +1,35 @@
 package com.moringa.lyrical_musicalapp.models;
 
-import org.parceler.TypeRangeParcelConverter;
+import android.os.Parcel;
 
-class ItemListParcelConverter implements TypeRangeParcelConverter {
-}
+import org.parceler.ParcelConverter;
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ItemListParcelConverter implements ParcelConverter<List<Track>> {
+    @Override
+    public void toParcel(List<Track> input, Parcel parcel) {
+        if (input == null) {
+            parcel.writeInt(-1);
+        }
+        else {
+            parcel.writeInt(input.size());
+            for (Track track : input) {
+                parcel.writeParcelable(Parcels.wrap(track), 0);
+            }
+        }
+    }
+
+    @Override
+    public List<Track> fromParcel(Parcel parcel) {
+        int size = parcel.readInt();
+        if (size < 0) return null;
+        List<Track> items = new ArrayList<Track>();
+        for (int i = 0; i < size; ++i) {
+            items.add((Track) Parcels.unwrap(parcel.readParcelable(Track.class.getClassLoader())));
+        }
+        return items;
+    }
+    }
