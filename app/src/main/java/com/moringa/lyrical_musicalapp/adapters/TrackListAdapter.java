642221f1336175e6be520cc9e021418818eeaacd
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 12:24:14 +0300

    Creted a tracklistAdapter Class that takes in Context and th list of Tracklist , Also Binded the textviews and Button for the seemoreDetails Also Implementated an onClick method and put Intent.putExtra for tracks Also implementated bindTrack method for tracklist and setText for all the TextViews

--- app/src/main/java/com/moringa/lyrical_musicalapp/adapters/TrackListAdapter.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/adapters/TrackListAdapter.java
@@ -1,17 +1,20 @@
 package com.moringa.lyrical_musicalapp.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringa.lyrical_musicalapp.R;
-import com.moringa.lyrical_musicalapp.models.Track;
+import com.moringa.lyrical_musicalapp.models.TrackList;
 import com.moringa.lyrical_musicalapp.ui.TrackDetailActivity;
 
 import org.parceler.Parcels;
@@ -21,78 +24,85 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-//import com.moringa.lyrical_musicalapp.ui.RDetailActivity;
-//import com.squareup.picasso.Picasso;
-//
-//import org.parceler.Parcels;
-
 public class TrackListAdapter extends RecyclerView.Adapter<TrackListAdapter.TrackViewHolder> {
-    private List<Track> mTracks;
+    private List<TrackList> mTracks;
+//    private Lyrics mLyrics;
     private Context mContext;
 
-    public TrackListAdapter(Context context, List<Track> tracks) {
+    public TrackListAdapter(Context context, List<TrackList> tracks) {
         mContext = context;
         mTracks = tracks;
     }
-
-//    public TrackListAdapter(TrackListActivity context, List<Track> track) {
+//    public TrackListAdapter(Context context,Lyrics lyrics) {
+//        mLyrics = lyrics;
 //    }
+
 
     @Override
     public TrackListAdapter.TrackViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view =
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.track_list_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.track_list_item, parent, false);
         TrackViewHolder viewHolder = new TrackViewHolder(view);
         return viewHolder;
     }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull TrackViewHolder holder, int position) {
-//
-//    }
 
 
     @Override
     public void onBindViewHolder(TrackListAdapter.TrackViewHolder holder, int position) {
         holder.bindTrack(mTracks.get(position));
+//        holder.bindLyrics(mLyrics);
 
     }
+
     @Override
     public int getItemCount() {
         return mTracks.size();
     }
 
     public class TrackViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        @BindView(R.id.q_trackTextView)
-        ImageView mTrackImageView;
-        @BindView(R.id.textView2)
-        TextView mNameTextView;
-        @BindView(R.id.categoryTextView) TextView mCategoryTextView;
-        @BindView(R.id.ratingTextView) TextView mRatingTextView;
+        @BindView(R.id.lyricsBodyTextView) TextView mLyricsTextView;
+        @BindView(R.id.textView2) TextView mNameTextView;
+        @BindView(R.id.trackRating) TextView mTrackRating;
+        @BindView(R.id.albumName) TextView mAlbumName;
+        @BindView(R.id.artistName) TextView mArtistName;
+        @BindView(R.id.trackShareUrl) TextView mTrackShareUrl;
+        @BindView(R.id.seeMoreDetails)
+        Button seeMoreDetails;
 
         private Context mContext;
 
         public TrackViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
-            itemView.setOnClickListener(this);
+
 
         }
 
         @Override
         public void onClick(View v) {
+            Log.d("Clicked this now", "Clicked");
+            Toast.makeText(this.mContext,"Clicked The View", Toast.LENGTH_SHORT).show();
             int itemPosition = getLayoutPosition();
             Intent intent = new Intent(mContext, TrackDetailActivity.class);
-            intent.putExtra("q_artist", itemPosition);
-            intent.putExtra("q_track", Parcels.wrap(mTracks));
+            intent.putExtra("tracks", Parcels.wrap(mTracks));
+//            intent.putExtra("tracks", Parcels.wrap(mLyrics));
+            intent.putExtra("position", itemPosition);
             mContext.startActivity(intent);
         }
 
-        public void bindTrack(Track track) {
-            mNameTextView.setText((CharSequence) track.getArtistName());
-            mRatingTextView.setText("Rating: " + track.getTrackShareUrl() + "/5");
-
+        @SuppressLint("SetTextI18n")
+        public void bindTrack(TrackList track) {
+            mNameTextView.setText(track.getTrack().getTrackName());
+            mTrackRating.setText("Rating: " + track.getTrack().getTrackRating() + "/100");
+            mAlbumName.setText(track.getTrack().getAlbumName());
+            mArtistName.setText(track.getTrack().getArtistName());
+            mTrackShareUrl.setText("Check Lyrics");
+            track.getTrack().getTrackShareUrl();
         }
+
+//        public void bindLyrics(Lyrics lyrics) {
+//            mLyricsTextView.setText(lyrics.getLyricsBody());
+//        }
     }
 }
