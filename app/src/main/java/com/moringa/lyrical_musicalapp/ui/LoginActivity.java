cd6c689ffa95420d2ffa6a8d4c31ae91ffa250e5
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 11:17:38 +0300

    Binded the RegisterTextView and implentented it in the OnClick method by creating and intent to the registerActivity

--- app/src/main/java/com/moringa/lyrical_musicalapp/ui/LoginActivity.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/ui/LoginActivity.java
@@ -1,12 +1,9 @@
 package com.moringa.lyrical_musicalapp.ui;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,77 +13,26 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-
-    EditText userNameTxt, passwordTxt;
-    Button signInBtn;
-    Context context = this;
-    @BindView(R.id.signInBtn)
-    Button getSignInBtn;
-    @BindView(R.id.userNameTxt)
-    EditText getUserNameTxt;
-    @BindView(R.id.registerBtn)
-    Button registerButton;
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         ButterKnife.bind(this);
-
-        final EditText userNameTxt = (EditText) findViewById(R.id.userNameTxt);
-        final EditText passwordTxt = (EditText) findViewById(R.id.passwordTxt);
-        final Button registerBtn = (Button) findViewById(R.id.registerBtn);
-        final Button sighInBtn = (Button) findViewById(R.id.signInBtn);
-
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
-            }
-        });
-
-        sighInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String userName = userNameTxt.getText().toString();
-                String password = passwordTxt.getText().toString();
-
-                if(userName.equals(""))
-                {
-                    Toast.makeText(getApplicationContext(),"Your UserName Is **Required**",
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(password.equals(""))
-                {
-                    Toast.makeText(getApplicationContext(),"Your Password Is **Required**",
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if (userName.equals("") && password.equals(""))
-                {
-
-                    Toast.makeText(getApplicationContext(),
-                            "Login Was Successful", Toast.LENGTH_LONG).show();
-                    Intent signInIntent = new Intent(LoginActivity.this, MainActivity.class);
-                    LoginActivity.this.startActivity(signInIntent);
-                }
-               else
-                {
-                    Toast.makeText(getApplicationContext(),
-                            "Account is not registered", Toast.LENGTH_LONG).show();
-                    Intent signInIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                    LoginActivity.this.startActivity(signInIntent);
-                }
-            }
-        });
+        mRegisterTextView.setOnClickListener(this);
 
     }
 
+
     @Override
     public void onClick(View view) {
-
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            LoginActivity.this.startActivity(intent);
+            finish();
+        }
     }
 }
