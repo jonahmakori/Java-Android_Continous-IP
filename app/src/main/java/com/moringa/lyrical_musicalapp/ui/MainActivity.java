3500d667c496bafe4d0c6779c66f1618299c021f
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 11:13:54 +0300

    Implented the SharedPreferences by decaring them at the top as private

--- app/src/main/java/com/moringa/lyrical_musicalapp/ui/MainActivity.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/ui/MainActivity.java
@@ -1,11 +1,15 @@
 package com.moringa.lyrical_musicalapp.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +18,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.moringa.lyrical_musicalapp.Constants;
 import com.moringa.lyrical_musicalapp.MusicGenresArrayAdapter;
 import com.moringa.lyrical_musicalapp.R;
 
@@ -21,6 +26,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    @BindView(R.id.viewSavedTracksButton)
+    Button mViewSavedTrackButton;
+    @BindView(R.id.searchTrack)
+    EditText searchTrackEditText;
+    @BindView(R.id.searchArtistEditText) EditText searchArtistEditText;
+    @BindView(R.id.submitTrackDetails)
+    Button searchTrackDetailsButton;
     @BindView(R.id.nav_view) BottomNavigationView bottomNavigationView;
     @BindView(R.id.listView)
     ListView mListView;
@@ -37,6 +52,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
 
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+
+        searchTrackDetailsButton.setOnClickListener(this);
+        mViewSavedTrackButton.setOnClickListener(this);
+
 //        mFindMusicCategoryButton.setOnClickListener(this);
         MusicGenresArrayAdapter adapter = new MusicGenresArrayAdapter(this, android.R.layout.simple_list_item_1, musicGenres);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListner);
@@ -44,12 +66,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                if (i == 0){
-                    startHipHop();
-                }else if(i == 1){
-                    startSoftRock();
-                }
                 String musicGenres = ((TextView) view).getText().toString();
                 Log.v(TAG,"In the onItemClickListener!");
                 Toast.makeText(MainActivity.this, musicGenres, Toast.LENGTH_LONG).show();
@@ -62,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             switch (menuItem.getItemId()){
                 case R.id.navigation_dashboard:
-                    startActivity(new Intent(MainActivity.this, SearchTrackActivity.class));
+                    startActivity(new Intent(MainActivity.this, MainActivity.class));
                     return true;
                 case R.id.navigation_home:
                     startActivity(new Intent(MainActivity.this, MainActivity.class));
@@ -71,18 +87,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     };
 
-    private void startSoftRock() {
-        Intent intent = new Intent(MainActivity.this, SoftRockActivity.class);
-        intent.putExtra("musicGenres", musicGenres);
-        startActivity(intent);
-    }
-
-
-    public void startHipHop(){
-        Intent intent = new Intent(MainActivity.this, HipHopActivity.class);
-        intent.putExtra("musicGenres", musicGenres);
-        startActivity(intent);
-    }
+//    private void startSoftRock() {
+//        Intent intent = new Intent(MainActivity.this, SoftRockActivity.class);
+//        intent.putExtra("musicGenres", musicGenres);
+//        startActivity(intent);
+//    }
+//
+//
+//    public void startHipHop(){
+//        Intent intent = new Intent(MainActivity.this, HipHopActivity.class);
+//        intent.putExtra("musicGenres", musicGenres);
+//        startActivity(intent);
+//    }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
@@ -92,6 +108,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        if(view == searchTrackDetailsButton){
+            String q_track = searchTrackEditText.getText().toString();
+            String q_artist = searchArtistEditText.getText().toString();
+            if((!(q_artist).equals("")) && (!(q_track).equals(""))) {
+                addToSharedPreferences(q_track, q_artist);
+            }
+            Log.d("Entered Track:", q_track);
+            Intent trackIntent = new Intent(MainActivity.this, TrackListActivity.class);
+            trackIntent.putExtra("q_track",q_track);
+            trackIntent.putExtra("q_artist",q_artist);
+            startActivity(trackIntent);
+        }
+        if(view == mViewSavedTrackButton){
+            Intent intent = new Intent(MainActivity.this, SavedTracksActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    private void addToSharedPreferences(String q_track,String q_artist) {
+        mEditor.putString(Constants.PREFERENCES_TRACK_KEY, q_track).apply();
+        mEditor.putString(Constants.PREFERENCES_ARTISTNAME_KEY, q_artist).apply();
 
     }
 }
