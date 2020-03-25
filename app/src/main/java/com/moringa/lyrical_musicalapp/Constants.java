c6f44081c3b7d59abd26ff03bd6823acaffdb343
jonahmakori <jonahmakori0@gmail.com>
2020-03-25 12:39:22 +0300

    Created a constants class where i set to public static final String of Base url,Api key, Track key ,ArtistName key, Child track

--- app/src/main/java/com/moringa/lyrical_musicalapp/Constants.java
+++ app/src/main/java/com/moringa/lyrical_musicalapp/Constants.java
@@ -3,5 +3,7 @@ package com.moringa.lyrical_musicalapp;
 public class Constants {
     public static final String MUSIXMATCH_BASE_URL = "https://api.musixmatch.com/";
     public static final String MUSIXMATCH_API_KEY = BuildConfig.MUSIXMATCH_API_KEY;
-
+    public static final String PREFERENCES_TRACK_KEY = "q_track" ;
+    public static final String PREFERENCES_ARTISTNAME_KEY = "q_artist" ;
+    public static final String FIREBASE_CHILD_TRACKS = "tracks";
 }
